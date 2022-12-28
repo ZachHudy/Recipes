697c5ddcd07b01279716066262dabf0ea10c5fdd
zachhudy <zachhudy@gmail.com>
2022-12-27 21:43:48 -0500

    Add strong typing to parameter in setCurrentPage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   currentPage: string;
 
-  setCurrentPage(page) {
+  setCurrentPage(page: string) {
     this.currentPage = page;
   }
 }
