d8dac1eccec5b35bc1ed6df1bfb9fe563798d291
zachhudy <zachhudy@gmail.com>
2022-12-27 21:31:42 -0500

    Add code to methods for setRecipes and setShopping list in Header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,12 +8,15 @@ import { Component, EventEmitter, Output } from "@angular/core";
 export class HeaderComponent {
     collapsed = true;
     @Output() setPage = new EventEmitter<string>();
+    currPage: string;
 
     setRecipes() {
-
+        this.currPage = 'recipes';
+        this.setPage.emit(this.currPage);
     }
 
     setShoppingList() {
-
+        this.currPage = 'shopping-list';
+        this.setPage.emit(this.currPage);
     }
 }
