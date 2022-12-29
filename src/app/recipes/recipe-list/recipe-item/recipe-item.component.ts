cdc0d006777c78e86959206d5b27a56f2edaf02f
zachhudy <zachhudy@gmail.com>
2022-12-28 21:11:55 -0500

    Add click event in recipe-item component to emit index of recipe

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -9,10 +9,15 @@ import { Recipe } from '../../recipe.model';
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
   @Input() thisIndex: number;
+  @Output() showRecipe = new EventEmitter<number>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectRecipe() {
+    this.showRecipe.emit(this.thisIndex);
   }
 
 }
