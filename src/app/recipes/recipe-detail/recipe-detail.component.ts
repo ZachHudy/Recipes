573e1edaedbaddd9c82ca86af864738884ded822
zachhudy <zachhudy@gmail.com>
2023-01-02 17:09:48 -0500

    Add recipe property for recipe detail

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeDetailComponent implements OnInit {
 
+  recipe: Recipe = new Recipe('Make a selection', 'select a recipe on the left', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
   constructor() { }
 
   ngOnInit(): void {
