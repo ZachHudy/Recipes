5cc5e2fb16bcc8e25ccaec41127138cb49bcc147
zachhudy <zachhudy@gmail.com>
2023-01-03 21:32:54 -0500

    Add method in shopping-list for addition of ingredient

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -18,4 +18,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onIngredientAdded(ingData: {ingName: string, ingAmount: number}) {
+    this.ingredients.push({name: ingData.ingName, amount: ingData.ingAmount})
+  }
+
 }
