979f9047b58ea2b238db26c55058e557abd5ac13
zachhudy <zachhudy@gmail.com>
2023-02-04 13:59:04 -0500

    Add recipes data from recipe-list to recipeservice

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
 
 @Injectable({providedIn: 'root'})
 
 export class RecipeService{
+    private recipes: Recipe[] = [
+        new Recipe('First Test Recipe', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'), new Recipe('Second Test Recipe', 'This is another test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+      ];
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
 
 }
