e688b2bfd73536953d2428dffd7af7b7a3011ed0
zachhudy <zachhudy@gmail.com>
2023-02-07 21:45:55 -0500

    Add ingredients property to recipe model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,4 +1,7 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class Recipe {
+    private ingredients: Ingredient[];
 
     constructor(public name: string, public description: string, public imagePath: string) {
 
