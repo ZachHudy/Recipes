acef44f1dfbcac47ebf02cd223225dffd1d34dc1
zachhudy <zachhudy@gmail.com>
2023-02-07 21:57:09 -0500

    Add ingredients to recipe; add ingredients list to recipe-detail template

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,9 +1,9 @@
 import { Ingredient } from "../shared/ingredient.model";
 
 export class Recipe {
-    private ingredients: Ingredient[];
+    
 
-    constructor(public name: string, public description: string, public imagePath: string) {
+    constructor(public name: string, public description: string, public imagePath: string, public ingredients: Ingredient[]) {
 
     }
 }
