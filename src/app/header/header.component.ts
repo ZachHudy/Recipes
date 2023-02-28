518ea6992487f3a093e1b87a70d2028647eb6549
zachhudy <zachhudy@gmail.com>
2023-02-27 21:24:07 -0500

    Remove methods no longer in use from header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "app-header",
@@ -7,16 +7,5 @@ import { Component, EventEmitter, Output } from "@angular/core";
 
 export class HeaderComponent {
     collapsed = true;
-    @Output() setPage = new EventEmitter<string>();
     currPage: string;
-
-    setRecipes() {
-        this.currPage = 'recipes';
-        this.setPage.emit(this.currPage);
-    }
-
-    setShoppingList() {
-        this.currPage = 'shopping-list';
-        this.setPage.emit(this.currPage);
-    }
 }
